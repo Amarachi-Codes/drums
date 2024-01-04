77859c634f5ff5214278891fd0bb8bc39101ccad
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-01-04 23:19:34 +0100

    debugged

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function playSound(e){
    const audio= document.querySelector(`audio[data-key ="${e.keyCode}"]`)
    const key =document.querySelector(`div[data-key="${e.keyCode}"]`);
    if(!audio) return;  //end it if theres no audio
-   key.classList.add(playing);
+   key.classList.add('playing');
    audio.currentTime = 0;
    audio.play()
 }
